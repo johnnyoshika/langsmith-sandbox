71bd0c4dafc55df8de9c06353fe4cae60cc43730
Johnny Oshika <johnnyoshika@gmail.com>
2024-06-03 14:51:37 -0700

    Add name and metadata

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,24 @@ app.get('/trace', async (req, res) => {
   // Auto-trace LLM calls in-context
   const client = wrapOpenAI(new OpenAI());
   // Auto-trace this function
-  const pipeline = traceable(async (message: string) => {
-    const result = await client.chat.completions.create({
-      messages: [
-        { role: 'system', content: 'You are a helpful chatbot' },
-        { role: 'user', content: message },
-      ],
-      model: 'gpt-3.5-turbo',
-    });
-    return result.choices[0].message.content;
-  });
+  const pipeline = traceable(
+    async (message: string) => {
+      const result = await client.chat.completions.create({
+        messages: [
+          { role: 'system', content: 'You are a helpful chatbot' },
+          { role: 'user', content: message },
+        ],
+        model: 'gpt-3.5-turbo',
+      });
+      return result.choices[0].message.content;
+    },
+    {
+      run_type: 'llm',
+      name: 'OpenAI Call Traceable',
+      tags: ['my-tag'],
+      metadata: { 'my-key': 'my-value' },
+    },
+  );
 
   const response = await pipeline(
     'What is the square root of 16? Provide the answer and an explantion.',
@@ -33,14 +41,42 @@ app.get('/trace', async (req, res) => {
 });
 
 app.get('/prompt', async (req, res) => {
+  async function retriever(query: string) {
+    return [
+      'Marcus the Pig stood at the edge of the rickety wooden bridge, his hooves trembling beneath him as he faced the dark, swirling waters below. The bridge, old and worn, groaned with every gust of wind, threatening to collapse under his weight. Across the chasm, the rest of the animals from the farm watched with bated breath, their eyes wide with fear and hope. It was up to Marcus to retrieve the magical acorn that would save their drought-stricken land, but the path was treacherous and the stakes had never been higher. He took a deep breath, summoning every ounce of courage, and stepped forward, the bridge swaying perilously under his weight.',
+      `Suddenly, a sharp crack echoed through the canyon as one of the bridge's planks snapped under Marcus's hoof. He teetered on the edge, his heart pounding wildly in his chest. "You can do it, Marcus!" shouted Bella the Duck from the other side, her voice barely audible over the roar of the river below. Summoning his last reserves of strength, Marcus lunged forward just as the bridge began to crumble behind him. He landed on the other side, the magical acorn clutched tightly in his mouth. Cheers erupted from the animals as Marcus lay panting on the ground, a hero who had risked everything to save his friends and their home.`,
+    ];
+  }
+
   const prompt = await hub.pull<ChatPromptTemplate>('blooms');
 
   const model = new ChatOpenAI();
   const runnable = prompt.pipe(model);
 
-  const result = await runnable.invoke({
-    question: 'What is 1 + 1?',
-  });
+  const rag = traceable(
+    async function rag(question: string) {
+      const docs = await retriever(question);
+
+      return await runnable.invoke(
+        {
+          question: `${docs.join('\n')}\n\n${question}`,
+        },
+        {
+          runName: 'Blooms Taxonomy Level',
+          metadata: { blooms: 'foo-bar' },
+        },
+      );
+    },
+    {
+      name: 'RAG',
+      tags: ['my-tag'],
+      metadata: { 'my-key': 'my-value' },
+    },
+  );
+
+  const result = await rag(
+    "How does Marcus's journey across the rickety bridge illustrate the themes of courage and self-sacrifice in the story?",
+  );
 
   const content = getTextFromMessageContent(result.content);
   const json = JSON.parse(content) as { level: number };
